07cbdb67d8876c0cbc1dabae42cd023314cda3f1
Matthew Robert Kilgour Booker <mrbooker@ghc52.ghc.andrew.cmu.edu>
2023-02-16 15:02:13 -0500

    Task 1 done (full points for correctness)

--- src/seq-simulator.cpp
+++ src/seq-simulator.cpp
@@ -10,16 +10,67 @@
 // be helpful.
 
 const int QuadTreeLeafSize = 8;
-class SequentialNBodySimulator : public INBodySimulator {
+class SequentialNBodySimulator : public INBodySimulator
+{
 public:
   std::unique_ptr<QuadTreeNode> buildQuadTree(std::vector<Particle> &particles,
-                                              Vec2 bmin, Vec2 bmax) {
+                                              Vec2 bmin, Vec2 bmax)
+  {
     // TODO: implement a function that builds and returns a quadtree containing
     // particles.
+    std::unique_ptr<QuadTreeNode> curNode(new QuadTreeNode);
+
+    if (particles.size() <= QuadTreeLeafSize)
+    { 
+      curNode->isLeaf = true;
+      curNode->particles = particles;
+      return curNode;
+    }
+    else
+    {
+      curNode->isLeaf = false;
+      Vec2 pivot;
+      pivot.x = (bmax.x + bmin.x) / 2;
+      pivot.y = (bmax.y + bmin.y) / 2;
+
+      std::vector<Particle> childVectors[4];
+
+      for(Particle &p : particles)
+      {
+        bool isLeft = p.position.x < pivot.x;
+        bool isUp = p.position.y < pivot.y;
+
+        if (isLeft && isUp)
+          childVectors[0].push_back(p);
+        else if (!isLeft && isUp)
+          childVectors[1].push_back(p);
+        else if (isLeft && !isUp)
+          childVectors[2].push_back(p);
+        else
+          childVectors[3].push_back(p);
+      }
+
+      // Create the subtrees for this node
+      curNode->children[0] = buildQuadTree(childVectors[0], bmin, pivot);
+
+      Vec2 topRightMin = {pivot.x, bmin.y};
+      Vec2 topRightMax = {bmax.x, pivot.y};
+      curNode->children[1] = buildQuadTree(childVectors[1], topRightMin, topRightMax);
+
+      Vec2 bottomLeftMin = {bmin.x, pivot.y};
+      Vec2 bottomLeftMax = {pivot.x, bmax.y};
+      curNode->children[2] = buildQuadTree(childVectors[2], bottomLeftMin, bottomLeftMax);
+
+      curNode->children[3] = buildQuadTree(childVectors[3], pivot, bmax);
+
+      return curNode;
+    }
+
     return nullptr;
   }
   virtual std::unique_ptr<AccelerationStructure>
-  buildAccelerationStructure(std::vector<Particle> &particles) {
+  buildAccelerationStructure(std::vector<Particle> &particles)
+  {
     // build quad-tree
     auto quadTree = std::make_unique<QuadTree>();
 
@@ -27,7 +78,8 @@ public:
     Vec2 bmin(1e30f, 1e30f);
     Vec2 bmax(-1e30f, -1e30f);
 
-    for (auto &p : particles) {
+    for (auto &p : particles)
+    {
       bmin.x = fminf(bmin.x, p.position.x);
       bmin.y = fminf(bmin.y, p.position.y);
       bmax.x = fmaxf(bmax.x, p.position.x);
@@ -39,7 +91,8 @@ public:
 
     // build nodes
     quadTree->root = buildQuadTree(particles, bmin, bmax);
-    if (!quadTree->checkTree()) {
+    if (!quadTree->checkTree())
+    {
       std::cout << "Your Tree has Error!" << std::endl;
     }
 
@@ -48,12 +101,27 @@ public:
   virtual void simulateStep(AccelerationStructure *accel,
                             std::vector<Particle> &particles,
                             std::vector<Particle> &newParticles,
-                            StepParameters params) override {
+                            StepParameters params) override
+  {
     // TODO: implement sequential version of quad-tree accelerated n-body
     // simulation here, using quadTree as acceleration structure
+    for (int i = 0; i < particles.size(); i++)
+    {
+      Particle curParticle = particles[i];
+
+      Vec2 force = Vec2(0.0f, 0.0f);
+      std::vector<Particle> nearbyParticles;
+      accel->getParticles(nearbyParticles, curParticle.position, params.cullRadius);
+
+      for (const Particle& nearbyP : nearbyParticles)
+        force += computeForce(curParticle, nearbyP, params.cullRadius);
+
+      newParticles[i] = updateParticle(curParticle, force, params.deltaTime);
+    }
   }
 };
 
-std::unique_ptr<INBodySimulator> createSequentialNBodySimulator() {
+std::unique_ptr<INBodySimulator> createSequentialNBodySimulator()
+{
   return std::make_unique<SequentialNBodySimulator>();
 }
